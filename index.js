b5b25345f9335a4c6333196c03530a1060ec969d
TinkerPal <tinkerpi2023@gmail.com>
2024-10-13 09:23:28 +0300

    make it better

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const allowedOrigin = [
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://socket-fi.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
