33363a02f4419aa7d1ebb78ba5e2a8b2e2abaccc
TinkerPal <tinkerpi2023@gmail.com>
2024-10-13 13:00:28 +0300

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cookieSession = require("cookie-session");
 const express = require("express");
 const app = express();
 const passport = require("passport");
@@ -21,28 +20,17 @@ mongoose
     console.error("Error connecting to mongo db:", err);
   });
 
-// app.use(
-//   cookieSession({
-//     name: "sessionSocketFI",
-//     keys: [keys.COOKIE_KEY],
-//     maxAge: 24 * 60 * 60 * 1000, // 24 hour
-//     // secure: true, // Enable only in production
-//     httpOnly: true, // Prevent client-side access
-//     sameSite: false, // Allow cookies in cross-site requests
-//   })
-// );
-
 app.use(
   session({
-    name: "sessionSocketFI", // Name of the session cookie
-    secret: keys.COOKIE_KEY, // Secret used to sign the session ID cookie
-    resave: false, // Forces the session to be saved back to the session store
-    saveUninitialized: false, // Forces a session that is new but unmodified to be saved to the store
+    name: "sessionSocketFI",
+    secret: keys.COOKIE_KEY,
+    resave: false,
+    saveUninitialized: false,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
-      // secure: true, // Only set cookies over HTTPS (true in production)
-      // httpOnly: true, // Prevent client-side access to cookies
-      // sameSite: "none", // Allow cookies in cross-site requests
+      secure: process.env.NODE_ENV === "production", // Only set cookies over HTTPS
+      httpOnly: true, // Prevent client-side access to cookies
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Cross-site in production, strict in dev
     },
   })
 );
@@ -50,29 +38,25 @@ app.use(
 // parse cookies
 app.use(cookieParser());
 
-// initalize passport
+// initialize passport
 app.use(passport.initialize());
-// deserialize cookie from the browser
 app.use(passport.session());
 
 const allowedOrigin = [
   "http://localhost:5173",
-  "http://www.localhost:5173",
   "http://localhost:4000",
   "https://auth-twitter.socket.fi",
+  "https://socket.fi",
 ];
 
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "https://www.socket.fi/",
+    origin: allowedOrigin,
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
-
-// app.options("*", cors());
 
 app.use("/auth", authRoutes);
 
@@ -80,7 +64,7 @@ const authCheck = (req, res, next) => {
   if (!req.user) {
     res.status(401).json({
       authenticated: false,
-      message: "user has not been authenticated",
+      message: "User has not been authenticated",
     });
   } else {
     next();
@@ -90,11 +74,10 @@ const authCheck = (req, res, next) => {
 app.get("/", authCheck, (req, res) => {
   res.status(200).json({
     authenticated: true,
-    message: "user successfully authenticated",
+    message: "User successfully authenticated",
     user: req.user,
     cookies: req.cookies,
   });
 });
 
-// connect react to nodejs express server
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
