3e60b62eb7c99ce806946b528dc2594e98195ba4
TinkerPal <tinkerpi2023@gmail.com>
2024-10-13 11:28:31 +0300

    make it better

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.use(
     name: "sessionSocketFI",
     keys: [keys.COOKIE_KEY],
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    path: "https://www.socket.fi",
     // secure: true, // Enable only in production
     httpOnly: true, // Prevent client-side access
     sameSite: false, // Allow cookies in cross-site requests
