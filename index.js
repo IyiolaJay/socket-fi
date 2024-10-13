45bf05c90b5f9fb2c7ffb994a085a300cd318f3b
TinkerPal <tinkerpi2023@gmail.com>
2024-10-13 13:08:35 +0300

    make it better

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
       // secure: process.env.NODE_ENV === "production", // Only set cookies over HTTPS
       httpOnly: true, // Prevent client-side access to cookies
-      // sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Cross-site in production, strict in dev
+      sameSite: false,
     },
   })
 );
