711524035787182939916efd0ab7a6d9549a0e6d
IyiolaJay <jimoh.iyiola11@gmail.com>
2024-10-14 10:13:47 +0100

    first push

--- config/keys.js
+++ config/keys.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 // const DB_PASSWORD = process.env.DB_PASSWORD;
 // const DB_USER = process.env.DB_USER;
-const COOKIE_KEY = "process.env.COOKIE_KEY";
+const COOKIE_KEY = process.env.COOKIE_KEY;
 
 const MONGODB = {
-  MONGODB_URI: `mongodb+srv://iyiola_dev:iyiola081719@cluster0.nfszgum.mongodb.net/solcurate-db-dev?retryWrites=true&w=majority`,
+  MONGODB_URI: process.env.MONGODB_URI,
 };
 
 const SESSION = {
